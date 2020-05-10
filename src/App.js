e9419a9fe414345005c5a117e3ae15cc03870094
materious <materious@gmail.com>
2020-05-10 14:42:30 +0900

    5강 customer loop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
+const customers= [
+{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'이은상',
+  'birthday':'760504',
+  'gender':'남자',
+  'job':'프리랜서'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'나동빈',
+  'birthday':'960508',
+  'gender':'남자',
+  'job':'디자이너'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'이순신',
+  'birthday':'961127',
+  'gender':'남자',
+  'job':'디자이너'
+}
+]
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo}  alt="logo" />
-         <h2>Let's develop management system!</h2>
+      <div>
+      {customers.map(c => {
+      return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+      })}
       </div>
+      
     );
   }
 }
